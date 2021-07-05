31da71448f1307086fe00e115be945761bc87bd6
Jacek Ziółkowski <s19086@pjwstk.edu.pl>
2021-07-05 10:40:09 +0200

    test

---
+++ FIFATests/EditTest/EditTest.cs
@@ -0,0 +1,61 @@
+using System;
+using EditTest.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace EditTest
+{
+    public class Tests
+    {
+        public IWebDriver driver;
+
+        public string url = "https://recruitment.fifatms.com/?desc=test";
+        
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var editPage = new EditPage(driver);
+            editPage.Driver.Navigate().GoToUrl(url);
+            var firstname = "sample";
+            var lastname = "sample";
+            editPage.insertData(firstname,lastname);
+            new WebDriverWait(driver, TimeSpan.FromMilliseconds(100))
+                .Until(ExpectedConditions.TextToBePresentInElement(editPage.FirstName, firstname));
+            new WebDriverWait(driver, TimeSpan.FromMilliseconds(100))
+                .Until(ExpectedConditions.TextToBePresentInElement(editPage.LastName, lastname));
+            Assert.That(editPage.FirstName.Text.Equals(firstname));
+            Assert.That(editPage.LastName.Text.Equals(lastname));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var editPage = new EditPage(driver);
+            editPage.Driver.Navigate().GoToUrl(url);
+            var firstname = Utils.Utils.RandomString(10);
+            var lastname = "ronaldo";
+            editPage.insertData(firstname,lastname);
+            editPage.Driver.Navigate().Refresh();
+            new WebDriverWait(driver, TimeSpan.FromMilliseconds(100))
+                .Until(ExpectedConditions.TextToBePresentInElement(editPage.FirstName, firstname));
+            Assert.That(editPage.FirstName.Text.Equals(firstname));
+            Assert.That(editPage.LastName.Text.Equals(lastname));
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Dispose();
+        }
+    }
+}
---
+++ FIFATests/EditTest/Pages/EditPage.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+
+namespace EditTest.Pages
+{
+    public class EditPage
+    {
+
+        public IWebDriver Driver { get; set; }
+        
+        [FindsBy(How=How.Id, Using = "edit-button")]
+        public IWebElement EditButton { get; set; }
+        
+        [FindsBy(How=How.Id, Using = "first-name")]
+        public IWebElement FirstName { get; set; }
+        
+        [FindsBy(How=How.Id, Using = "last-name")]
+        public IWebElement LastName { get; set; }
+        
+        [FindsBy(How=How.Id, Using = "first-name-edit")]
+        public IWebElement FirstNameField { get; set; }
+        
+        [FindsBy(How=How.Id, Using = "last-name-edit")]
+        public IWebElement LastNameField { get; set; }
+        
+        [FindsBy(How=How.Id, Using = "save-button")]
+        public IWebElement SaveButton { get; set; }
+
+        public EditPage(IWebDriver driver)
+        {
+            this.Driver = driver;
+            PageFactory.InitElements(driver,this);
+        }
+
+        public void insertData(string firstname, string lastname)
+        {
+            EditButton.Click();
+            FirstNameField.SendKeys(firstname);
+            LastNameField.SendKeys(lastname);
+            SaveButton.Click();
+        }
+    }
+}
